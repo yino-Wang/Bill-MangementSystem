e3138de0e2989859134a01c9ef6861505b053d4e
yino-Wang <yinooo66688@gmail.com>
2025-04-26 13:52:57 +1000

    TabBar implemented

--- src/pages/layout/index.js
+++ src/pages/layout/index.js
@@ -1,24 +1,59 @@
-import { Outlet } from "react-router-dom"
-import { Button } from "antd-mobile"
+import { TabBar } from "antd-mobile"
 import { useEffect } from "react"
-import { useDispatch } from "react-redux"
-import { getBillList } from "../../store/modules/billStore"
-const Layout = () => {
-    const dispatch = useDispatch()
-    useEffect(() => {
-        dispatch(getBillList())
-    },[dispatch])
-    return (
-        <div>
-            Layout page
-            <Outlet/>
-            <Button color="primary">test</Button>
-            <div className="purple">
-            <Button color="primary">test</Button>
-            </div>
-        </div>
+import { Outlet, useNavigate } from "react-router-dom"
+import { useDispatch } from 'react-redux'
+import { getBillList } from "@/store/modules/billStore"
+import './index.css'
+import {
+  BillOutline,
+  CalculatorOutline,
+  AddCircleOutline
+} from 'antd-mobile-icons'
 
-    )
+const tabs = [
+  {
+    key: '/month',
+    title: '月度账单',
+    icon: <BillOutline />,
+  },
+  {
+    key: '/new',
+    title: '记账',
+    icon: <AddCircleOutline />,
+  },
+  {
+    key: '/year',
+    title: '年度账单',
+    icon: <CalculatorOutline />,
+  },
+]
+
+const Layout = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(getBillList())
+  }, [dispatch])
+
+  // 切换菜单跳转路由
+  const navigate = useNavigate()
+  const swithRoute = (path) => {
+    console.log(path)
+    navigate(path)
+  }
+  return (
+    <div className="layout">
+      <div className="container">
+        <Outlet />
+      </div>
+      <div className="footer">
+        <TabBar onChange={swithRoute}>
+          {tabs.map(item => (
+            <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+          ))}
+        </TabBar>
+      </div>
+    </div>
+  )
 }
 
 export default Layout
