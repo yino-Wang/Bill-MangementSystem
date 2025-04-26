5c8c8e7d78f50e6a1a315d843358aeeb6c33ddc8
yino-Wang <yinooo66688@gmail.com>
2025-04-26 17:06:35 +1000

    final monthly stastics implemented

--- src/pages/Month-bill/index.js
+++ src/pages/Month-bill/index.js
@@ -1,14 +1,71 @@
 import { NavBar, DatePicker } from 'antd-mobile'
 import './index.css'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useMemo } from 'react'
 import classNames from 'classnames'
+import dayjs from 'dayjs'
+import { useSelector } from 'react-redux'
+import _ from 'lodash'
 
 const Month = () => {
+    //按月做数据的分组
+    const billList = useSelector(state => state.bill.billList)
+    //数据二次处理useMemo lodash库的groupBy用来分组
+    const monthGroup = useMemo(() => {
+        const grouped = _.groupBy(billList, (item) => {
+          console.log('item.date', item.date)
+          return dayjs(item.date).format('YYYY-MM')
+        })
+        console.log('最终生成的monthGroup keys:', Object.keys(grouped))
+        return grouped
+      }, [billList])
+
     //控制弹框打开和关闭
     const [dataVisible,setDataVisible] = useState(false)
-    const onClose = () => {
+    
+    // 控制时间显示
+    const [currentDate, setCurrentDate] = useState(() => {
+        return dayjs(new Date()).format('YYYY-MM')
+    })
+
+    const [currentMonthList, setCurrentMonthList] = useState([])
+
+    const monthResult = useMemo(() => {
+        // 支出  /  收入  / 结余
+        const pay = currentMonthList.filter(item => item.type === 'pay').reduce((a, c) => a + c.money, 0)
+        const income = currentMonthList.filter(item => item.type === 'income').reduce((a, c) => a + c.money, 0)
+        return {
+          pay,
+          income,
+          total: pay + income
+        }
+    
+    }, [currentMonthList])
+    
+    //
+    useEffect(() => {
+        const nowDate = dayjs().format('YYYY-MM')
+        //边界值控制
+        if(monthGroup[nowDate]){
+            setCurrentMonthList = (monthGroup[nowDate])
+        }
+    },[monthGroup])
+
+    //确认回调
+    const onComfirm = (date) => {
         setDataVisible(false)
+        // 其他逻辑
+        console.log(date)
+        const formatDate = dayjs(date).format('YYYY-MM')
+        console.log(formatDate)
+        setCurrentMonthList(monthGroup[formatDate] || [])
+        setCurrentDate(formatDate) 
+        console.log('当前选择的formatDate:', formatDate)
+        console.log('monthGroup中有哪些key:', Object.keys(monthGroup))
+        console.log('拿到的账单:', monthGroup[formatDate])
+
     }
+
   return (
     <div className="monthlyBill">
       <NavBar className="nav" backArrow={false}>
@@ -19,7 +76,7 @@ const Month = () => {
           {/* 时间切换区域 */}
           <div className="date" onClick={() => setDataVisible(true)}>
             <span className="text">
-              2023 | 3月账单
+              {currentDate + ''}月账单
             </span>
             {/**/}
             <span className= {classNames('arrow', dataVisible && 'expand')}></span>
@@ -28,15 +85,15 @@ const Month = () => {
           <div className='twoLineOverview'>
             <div className="item">
               <span className="money">{100}</span>
-              <span className="type">支出</span>
+              <span className="type">{monthResult.pay.toFixed(2)}</span>
             </div>
             <div className="item">
               <span className="money">{200}</span>
-              <span className="type">收入</span>
+              <span className="type">{monthResult.income.toFixed(2)}</span>
             </div>
             <div className="item">
               <span className="money">{200}</span>
-              <span className="type">结余</span>
+              <span className="type">{monthResult.total.toFixed(2)}</span>
             </div>
           </div>
           {/* 时间选择器 */}
@@ -46,8 +103,8 @@ const Month = () => {
             precision="month"
             visible={dataVisible}
             onCancel={() => setDataVisible(false)}
-            onConfirm={() => setDataVisible(false)}
-            onClose={onClose} //点击盲层关闭
+            onConfirm={onComfirm}
+            onClose={() => setDataVisible(false)} //点击盲层关闭
             max={new Date()}
           />
         </div>
